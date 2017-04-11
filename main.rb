d7a18714a7c54fdbd317650a11f150bbdebc2960
Brendan Walker <brenny_walker@hotmail.com>
2017-04-11 03:15:45 +0000

    Adds custom user names

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require_relative 'game'
 require_relative 'player'
 require_relative 'questions'
 
-new_game = Game.new(Player.new("Brendan"), Player.new("Plumbus"))
+puts "Player 1, please enter your name:"
+player1 = gets.chomp
+
+puts "Player 2, please enter your name:"
+player2 = gets.chomp
+
+new_game = Game.new(Player.new(player1), Player.new(player2))
 
 new_game.start_game
